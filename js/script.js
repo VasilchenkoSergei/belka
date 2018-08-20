e5d55e9ffedd0e2c8993cdf2bfd28b8b5d4cf46a
VasilchenkoSergei <chainsaw86@mail.ru>
2018-08-20 22:13:37 +0300

    change slider, menu to do

--- js/script.js
+++ js/script.js
@@ -1,70 +1,70 @@
 $(document).ready(function(){
 
 
+  $('.routes__list').slick({
+    arrows: false,
+    centerMode: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+  });
 
-$('')
+  $('.pictures__container').slick({
+    arrows: false,
+    slidesToShow: 3,
+        responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+  });
 
-  // $('.promo__burger').bind('click', function() {
-  // $(this).toggleClass('active');
-  // $(this).find('div').removeClass('no-animation');
-  // $(".page-header").toggle();
-  //   });
-
-  // $('.routes__list').slick({
-  //   arrows: true,
-  //   slidesToShow: 5,
-  //   slidesToScroll: 1,
-  //   prevArrow: '<button type="button"><img src="../img/icons/arrow-prev.png"></button>',
-  //   nextArrow: '<button type="button"><img src="../img/icons/arrow-next.png"></button>',
-  //   responsive: [
-  //     {
-  //       breakpoint: 1200,
-  //       settings: {
-  //         slidesToShow: 4,
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         slidesToShow: 3,
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         centerMode: true,
-  //         slidesToShow: 1,
-  //       }
-  //     }
-  //   ]
-  // });
 
   // $('.reviews__list').slick({
   //   arrows: false,
+  //   centerMode: true,
   //   slidesToShow: 3,
   //   responsive: [
+  //     // {
+  //     //   breakpoint: 1050,
+  //     //   settings: {
+  //     //     slidesToShow: ,
+  //     //   }
+  //     // },
   //     {
-  //       breakpoint: 480,
+  //       breakpoint: 768,
   //       settings: {
   //         centerMode: true,
   //         slidesToShow: 1,
   //       }
-  //     }
+  //     },
+  //     // {
+  //     //   breakpoint: 480,
+  //     //   settings: {
+  //     //     centerMode: true,
+  //     //     slidesToShow: 1,
+  //     //   }
+  //     // }
   //   ]
   // });
 
-
-  // $('.pictures__container').slick({
-  //   arrows: false,
-  //   responsive: [
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         centerMode: true,
-  //         slidesToShow: 1,
-  //       }
-  //     }
-  //   ]
-  // });
 
 });
