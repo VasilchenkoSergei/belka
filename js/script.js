09ad3aae6955dd59b9c3a13ac88f0fe844af3225
VasilchenkoSergei <chainsaw86@mail.ru>
2018-10-14 00:48:11 +0300

    some fixes

--- js/script.js
+++ js/script.js
@@ -9,16 +9,20 @@ $('.routes__list').owlCarousel({
     loop:true,
     centerMode:true,
     margin:10,
-    // nav:true,
+    dots: false,
+    nav: true,
+    navText: ["<img src='img/icons/arrow-prev.png'>", "<img src='img/icons/arrow-next.png'>"],
     responsive:{
         0:{
             items:1
         },
-        768:{
-            items:3
+        767:{
+            items:3,
+
         },
-        1200:{
-            items:5
+        1199:{
+            items:5,
+            nav: false,
         }
     }
 });
@@ -27,14 +31,14 @@ $('.pictures__container').owlCarousel({
     loop:true,
     centerMode:true,
     margin:10,
-    // nav:true,
+    dots: false,
     responsive:{
         0:{
-            items:1
+            items:1,
         },
-        768:{
-            items:3
-        },
+        767:{
+            items:3,
+        }
     }
 });
 
@@ -42,77 +46,35 @@ $('.reviews__list').owlCarousel({
     loop:true,
     centerMode:true,
     margin:10,
-    // nav:true,
+    dots: false,
     responsive:{
         0:{
             items:1
         },
-        768:{
-            items:3
+        767:{
+            items:3,
         },
     }
 });
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin:10,
-//     // nav:true,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         800:{
-//             items:3
-//         },
-//         1100:{
-//             items:5
-//         }
-//     }
-// })
-// new Siema({
-//   selector: '.siema',
-//   duration: 200,
-//   easing: 'ease-out',
-//   perPage: 3,
-//   draggable: true,
-//   threshold: 20,
-//   loop: true,
-//   onInit: () => {},
-//   onChange: () => {},
-// });
-  // $('.routes__list').slick({
-  //   arrows: false,
-  //   centerMode: true,
-  //   slidesToShow: 5,
-  //   slidesToScroll: 5,
-  //   responsive: [
-  //     {
-  //       breakpoint: 1200,
-  //       settings: {
-  //         slidesToShow: 3,
-  //         slidesToScroll: 3,
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         slidesToShow: 1,
-  //       }
-  //     },
-  //   ]
-  // });
 
-  // $('.pictures__container').slick({
-  //   arrows: false,
-  //   slidesToShow: 3,
-  //       responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         slidesToShow: 1,
-  //       }
-  //     },
-  //   ]
-  // });
+
+$(window).width(function() {
+  if ($(window).width() > 767) {
+    $('.pictures__container').trigger('destroy.owl.carousel');
+  }
+  else {
+    return false;
+  }
+});
+
+
+
+
+
+
+
+
+
 
 
 });
